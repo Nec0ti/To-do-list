3957c4c942050a85c8262ad2ed304fb41248796f
Necoti <halilnecatig2@gmail.com>
2023-12-15 19:24:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,78 @@
+var taskList = JSON.parse(localStorage.getItem("taskList")) || [];
+
+// Function to render tasks in the DOM
+function renderTasks() {
+    var taskListElement = document.getElementById("taskList");
+    taskListElement.innerHTML = "";
+
+    taskList.forEach(function (task) {
+        var li = document.createElement("li");
+        var deleteButton = document.createElement("button");
+
+        li.textContent = `${task.description} - ${task.date}`;
+        
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", function () {
+            deleteTask(task);
+        });
+
+        li.appendChild(deleteButton);
+        taskListElement.appendChild(li);
+    });
+}
+
 function addTask() {
-  var taskInput = document.getElementById("taskInput");
-  var taskDateInput = document.getElementById("taskDate");
-  var taskList = document.getElementById("taskList");
+    var taskInput = document.getElementById("taskInput");
+    var taskDateInput = document.getElementById("taskDate");
 
-  if (taskInput.value.trim() === "" || taskDateInput.value === "") {
-      alert("Please enter a task and select a date!");
-      return;
+    if (taskInput.value.trim() === "" || taskDateInput.value === "") {
+        alert("Please enter a task and select a date!");
+        return;
+    }
+
+    var newTask = {
+        description: taskInput.value,
+        date: taskDateInput.value
+    };
+
+    taskList.push(newTask);
+
+    // Save the updated task list to local storage
+    localStorage.setItem("taskList", JSON.stringify(taskList));
+
+    // Render the updated task list in the DOM
+    renderTasks();
+
+    taskInput.value = "";
+    taskDateInput.value = "";
+
+    // Set a timer to send a notification when the task's due time is reached
+    scheduleNotification(newTask);
+}
+
+function deleteTask(task) {
+  var taskIndex = taskList.indexOf(task);
+
+  if (taskIndex !== -1) {
+      taskList.splice(taskIndex, 1);
+
+      // Save the updated task list to local storage
+      localStorage.setItem("taskList", JSON.stringify(taskList));
+
+      // Render the updated task list in the DOM
+      renderTasks();
   }
+}
 
-  var li = document.createElement("li");
-  var deleteButton = document.createElement("button");
 
-  li.textContent = `${taskInput.value} - ${taskDateInput.value}`;
-
-  deleteButton.textContent = "Delete";
-  deleteButton.addEventListener("click", function () {
-      this.parentNode.remove();
-  });
-
-  li.appendChild(deleteButton);
-
-  taskList.appendChild(li);
-  taskInput.value = "";
-  taskDateInput.value = "";
-
-  // Parse the selected date and time
-  var selectedDate = new Date(taskDateInput.value);
-  
-  // Set a timer to send a notification when the selected date and time is reached
+function scheduleNotification(task) {
+  var selectedDate = new Date(task.date);
   var currentTime = new Date().getTime();
   var timeToNotification = selectedDate - currentTime;
 
   if (timeToNotification > 0) {
       setTimeout(function () {
-          showNotification("Task Reminder", `Task "${taskInput.value}" is due!`);
+          showNotification("Task Reminder", `Task "${task.description}" is due!`);
       }, timeToNotification);
   } else {
       alert("Please select a future date and time for the task!");
@@ -58,3 +97,6 @@ function showNotification(title, message) {
       }
   }
 }
+
+
+renderTasks();
